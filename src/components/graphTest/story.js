e547357716c87f4f1dc4be1cce34838497ab98ba
hikkary <z.kerkeb@gmail.com>
2019-01-15 20:40:21 +0100

    refacto(graphTest): stories refacto

--- src/components/graphTest/story.js
+++ src/components/graphTest/story.js
@@ -3,9 +3,6 @@ import { storiesOf } from '@storybook/react'
 import Graph from './index'
 import Area from './areaGraph'
 
-import Bg from '../../static/bg.jpg'
-
 storiesOf('Graph', module)
   .add('Vertical Bar', () => <Graph />)
-  .add('Vertical Bar Background', () => <Graph background={Bg} />)
   .add('Area', () => <Area />)
