85a6895ef179f5c7024f15ec0eaeb00f2d0b3713
hikkary <z.kerkeb@gmail.com>
2019-01-22 18:14:58 +0100

    feat(loginForm): add verification

--- src/components/loginForm/index.js
+++ src/components/loginForm/index.js
@@ -1,36 +1,103 @@
+import { withRouter } from 'react-router-dom'
+import Proptypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import StyledLink from '../styledLink'
-import { SubTitle, Title } from '../texts'
+import { isEmail, isPassword } from '../../utils/regexs'
+import { SubTitle, Title, CommonText } from '../texts'
 import Input from '../input'
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  min-width: 350px;
   text-align: center;
 `
 
 const LoginSubtitle = styled(SubTitle)`
-  padding: 5px;
   cursor: pointer;
+  padding: 5px;
+`
+const StyledSubmit = styled(Input)`
+  background-color: transparent;
+  border: none;
+  color: ${props => props.theme.color.primaryColor};
 `
 
-export default class LoginForm extends React.Component {
+class LoginForm extends React.Component {
+  static propTypes = {
+    history: Proptypes.object
+  }
+  state = {
+    email: '',
+    emailError: '',
+    password: '',
+    passwordError: ''
+  }
+
+  clearErrorMessages = () => {
+    this.setState({ emailError: '', passwordError: '' })
+  }
+
+  inputHandler = event => {
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      console.log(this.state)
+    })
+  }
+
+  checkInputs = () => {
+    if (!isEmail(this.state.email)) {
+      this.setState({ emailError: 'Veuillez entrer un email correct' })
+      return false
+    }
+    if (!isPassword(this.state.password)) {
+      this.setState({
+        passwordError: 'Votre mot de passe doit faire au moins 8 characteres'
+      })
+      return false
+    }
+    return true
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { history } = this.props
+    this.clearErrorMessages()
+
+    if (!this.checkInputs()) {
+      console.log('err')
+      return
+    }
+    // mettre axios ici
+    console.log(this.state.email, this.state.password)
+    history.push('/home')
+  }
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <Title>Connexion</Title>
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Mot De passe" />
-        <StyledLink to="/home">
-          <LoginSubtitle uppercase>Se Connecter</LoginSubtitle>
-        </StyledLink>
+        <Input
+          onChange={this.inputHandler}
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <CommonText>{this.state.emailError}</CommonText>
+        <Input
+          onChange={this.inputHandler}
+          type="password"
+          placeholder="Mot De passe"
+          name="password"
+        />
+        <CommonText>{this.state.passwordError}</CommonText>
+        <StyledSubmit type="submit" value="SE CONNECTER" />
         <LoginSubtitle color="gray">Vous êtes nouveau ?</LoginSubtitle>
-        <LoginSubtitle color="gray">
-          Vous avez oublier votre mot de passe ?
-        </LoginSubtitle>
       </Form>
     )
   }
 }
+
+const ConnectedLoginForm = withRouter(LoginForm)
+
+export default ConnectedLoginForm
