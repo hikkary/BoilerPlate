ff70fe73b0f2e6189d92944c91574e3d9ee99b85
hikkary <z.kerkeb@gmail.com>
2019-01-27 17:15:24 +0100

    feat(themes): Dark and Light Theme Created and added to Layout

--- src/containers/layout.js
+++ src/containers/layout.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import Header from '../components/header'
+
+const Container = styled.div`
+  background-color: ${props => props.theme.color.backgroundColor};
+  min-height: 100vh;
+`
 
 export default class Layout extends React.Component {
   static propTypes = {
@@ -10,10 +16,10 @@ export default class Layout extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <Container>
         <Header />
         {this.props.children}
-      </React.Fragment>
+      </Container>
     )
   }
 }
