ac4c0577ec4d3700c6f19ed3244fe9ced296e69e
hikkary <z.kerkeb@gmail.com>
2019-02-28 21:08:00 +0100

    feat(routes): Private routes implemented

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
 import Login from '../containers/login'
 import HomePage from '../containers/homePage'
 import HouseCharacters from '../containers/houseCharacters'
 import Ranking from '../containers/ranking'
 import Options from '../containers/options'
+import PrivateRoute from '../utils/privateRoute'
 
 export default class Routes extends React.Component {
   render() {
@@ -13,10 +19,11 @@ export default class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/house/:houseName" component={HouseCharacters} />
-          <Route path="/ranking" component={Ranking} />
-          <Route path="/options" component={Options} />
+          <PrivateRoute path="/home" component={HomePage} />
+          <PrivateRoute path="/house/:houseName" component={HouseCharacters} />
+          <PrivateRoute path="/ranking" component={Ranking} />
+          <PrivateRoute path="/options" component={Options} />
+          <Redirect to="/" />
         </Switch>
       </Router>
     )
