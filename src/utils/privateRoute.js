6c7e4a7f20614b792e4ab6bcf61a77474b1801f8
hikkary <z.kerkeb@gmail.com>
2019-03-01 18:39:04 +0100

    fix(privateRoute): quick fix error in proptypes

--- src/utils/privateRoute.js
+++ src/utils/privateRoute.js
@@ -22,7 +22,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 )
 
 PrivateRoute.propTypes = {
-  component: PropTypes.node,
+  component: PropTypes.any,
   location: PropTypes.any
 }
 
